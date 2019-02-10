1c4a6da9574cd7db15a8ffc9813e5b7465b45009
swemeshy <46273722+swemeshy@users.noreply.github.com>
2019-02-10 13:40:07 -0800

    forgot to add db js files

---
+++ src/db_main_page.js
@@ -0,0 +1,42 @@
+var args = [];
+
+process.argv.forEach((val, index) => {
+  args.push(toLowerCase(val));
+});
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "112358F1B!",
+  database: "textbooks"
+});
+
+// First: search bar, second: filter1, third: filter2, etc.
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+if (args.length == 1) {
+  var keywords = args[0].split(" ", args[0].length);
+  var sql = "select * from textbooks.textbooks";
+  for (i = 0; i < keywords.length; i++) {
+    if (keywords[i] == "and") {
+      sql.concat(" and");
+    }
+    else {
+      var like = '%' + keywords[i] + '%';
+      sql.concat(" where PostName like " + like + " or");
+    }
+  }
+  sql.concat(';');
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    // handle error in front end by saying invalid search
+    // and specifying correct format of search
+    console.log("1 record inserted");
+  });
+}
